d93825194edb47b499716a93dad22f6da853723b
Dhruv Grandhi <dhruvgrandhi@outlook.com>
2022-02-07 17:41:37 -0800

    Added functionality to generateTowers() in main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,14 @@ basic.forever(function () {
 })
 
 
-function generateTowers(maxHeight: number) {
+function generateTowers(maxHeight: number, towerCount : number) : number[] {
+    let towers : number[] = [];
     
+    for (let counter = 0; counter < towerCount; counter++) {
+        // Rounds a random number and adds it to the towers array
+        let newHeight: number = Math.round(Math.random() * maxHeight);
+        towers.push(newHeight);
+    }
+
+    return towers;
 }
