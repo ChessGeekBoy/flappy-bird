9336cce1165694ca4346e730f55df1d0c8a428df
Dhruv Grandhi <dhruvgrandhi@outlook.com>
2022-02-07 17:44:07 -0800

    Created towerManager.ts and moved generateTowers() to it

---
+++ towerManager.ts
@@ -0,0 +1,11 @@
+function generateTowers(maxHeight: number, towerCount : number) : number[] {
+    let towers : number[] = [];
+    
+    for (let counter = 0; counter < towerCount; counter++) {
+        // Rounds a random number and adds it to the towers array
+        let newHeight: number = Math.round(Math.random() * maxHeight);
+        towers.push(newHeight);
+    }
+
+    return towers;
+}
