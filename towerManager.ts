f30e9a0c17606cf498968054c19f941472cbfb6c
Dhruv Grandhi <dhruvgrandhi@outlook.com>
2022-02-10 09:27:23 -0800

    moved functions to TowerManager class

--- towerManager.ts
+++ towerManager.ts
@@ -1,39 +1,35 @@
 namespace towerManagement {
-    export function generateTowers(maxHeight: number, towerCount: number): number[] {
-        let towers: number[] = [];
-    
-        for (let counter = 0; counter < towerCount; counter++) {
-            // Rounds a random number and adds it to the towers array
-            let newHeight: number = Math.round(Math.random() * maxHeight);
-            towers.push(newHeight);
-        }
-
-        return towers;
-    }
-
-    export function renderTowers(towerHeights: number[]): void {
-        
-    }
-
-    export function toPixelString(tower: number): string {
-        let pixelString: string = "";
-
-
-
-        return pixelString;
-    }
-
     export class TowerManager {
         screenHeight : number;
         onPixel: string;
         offPixel: string;
+        towerCount: number;
 
-
-
-        constructor(screenHeight : number, onPixel : string, offPixel : string ) {
+        constructor(screenHeight : number, onPixel : string, offPixel : string, towerCount : number) {
             this.screenHeight = screenHeight;
             this.onPixel = onPixel;
             this.offPixel = offPixel;
+            this.towerCount = towerCount;
+        }
+
+        toPixelString() {
+
+        }
+
+        renderTowers() {
+
+        }
+
+        generateTowers() : number[] {
+            let towers: number[] = [];
+    
+            for (let counter = 0; counter < this.towerCount; counter++) {
+                // Rounds a random number and adds it to the towers array
+                let newHeight: number = Math.round(Math.random() * this.screenHeight);
+                towers.push(newHeight);
+            }
+
+            return towers;
         }
     }
 }
