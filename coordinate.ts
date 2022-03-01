5d04a272c6f8409f6593af19a84bb62cf5792145
Dhruv Grandhi <dhruvgrandhi@outlook.com>
2022-03-01 10:16:42 -0800

    Improved aesthetic

--- coordinate.ts
+++ coordinate.ts
@@ -3,7 +3,7 @@ namespace gameEngine {
         x : number;
         y: number;
 
-        constructor(x: number, y: number) {
+        constructor(x : number, y : number) {
             this.x = x;
             this.y = y;
         }
