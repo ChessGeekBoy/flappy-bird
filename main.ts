b72f5549feb17487adf8924fba30d7f988ff679f
Spicy-Axolotl <michael.eremenko.personal@gmail.com>
2022-03-04 12:00:29 -0800

    Added init code.

--- main.ts
+++ main.ts
@@ -1,4 +1,27 @@
+let debug = false;
+
+//set game difficulty.
+gameEngine.Difficulty.Easy;
+if (debug) {
+    //tone to show difficulty has been set
+    music.playTone(196, music.beat(BeatFraction.Whole))
+}
+//init gravity
+gameEngine.Physics.prototype.initializeGravity();
+if (debug) {
+    //tone to show gravity has been inited and has worked
+    music.playTone(247, music.beat(BeatFraction.Whole))
+}
+//gen and render towers array
+gameEngine.TowerManager.prototype.generateTowers();
+gameEngine.TowerManager.prototype.renderTowers();
+if (debug) {
+    //tone to show towers array has worked
+    music.playTone(185, music.beat(BeatFraction.Whole))
+}
+
+
 basic.forever(function () {
-    // used to see if program runs
-    basic.showString("Hello world! Test.")
+    //used to see if program runs
+    basic.showString("Hello world! Test.");
 })
