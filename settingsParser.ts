ea16ea718c5d5f1774aa10ab1aa1c2d41c5d0b2b
Dhruv Grandhi <dhruvgrandhi@outlook.com>
2022-03-23 12:47:17 -0700

    Added some functionality to SettingParser

--- settingsParser.ts
+++ settingsParser.ts
@@ -1,7 +1,11 @@
 namespace gameEngine {
     export class SettingsParser {
         static parseSettings(json: string) : void {
-            let object = JSON.parse(json);
+            let object : any = JSON.parse(json);
+            let birdPosition: Coordinate
+                = new Coordinate(object["birdPosition"]["x"],
+                    object["birdPosition"]["y"]);
+            game.createSprite(birdPosition.x, birdPosition.y);
         }
     }
 }
